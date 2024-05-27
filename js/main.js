47be12e728968bc105c3c96738722b0c16271fb8
PEACE-DFG <awofesobipeace@gmail.com>
2024-05-28 00:33:22 +0100

    Remains Goals,Team,Contact,Review,Pricing Module

---
+++ js/main.js
@@ -0,0 +1,100 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.nav-bar').addClass('sticky-top');
+        } else {
+            $('.nav-bar').removeClass('sticky-top');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 24,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:2
+            }
+        }
+    });
+    
+})(jQuery);
+
+
+$(document).ready(function (e) {
+    $("#video-icon").on("click", function (e) {
+      e.preventDefault();
+      $(".video-popup").css("display", "flex");
+      $(".iframe-src").slideDown();
+    });
+    $(".video-popup").on("click", function (e) {
+      var $target = e.target.nodeName;
+      var video_src = $(this).find("iframe").attr("src");
+      if ($target != "IFRAME") {
+        $(".video-popup").fadeOut();
+        $(".iframe-src").slideUp();
+        $(".video-popup iframe").attr("src", " ");
+        $(".video-popup iframe").attr("src", video_src);
+      }
+    });
+
+  });
